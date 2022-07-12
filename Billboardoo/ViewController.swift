7eb34d57fe3dc84aad257b88c13afa23514ecb54
yongbeomkwak <yongbeomkwak@gmail.com>
2022-07-12 19:29:56 +0900

    Init Project

---
+++ Billboardoo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Billboardoo
+//
+//  Created by yongbeomkwak on 2022/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
