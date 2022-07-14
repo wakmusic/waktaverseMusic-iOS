d4e326e44e037083fc5c6c3a7ab2ea086fd0c8e9
yongbeomkwak <yongbeomkwak@gmail.com>
2022-07-14 21:47:33 +0900

    Implement navigationItem

--- Billboardoo/MainViewController.swift
+++ Billboardoo/MainViewController.swift
@@ -12,36 +12,46 @@ class MainViewController: UIViewController {
     
     var flag: Bool = true
     
+    //- MARK: IBOUT
+    
+    
+    
     //- MARK: MemberFunc
-    func changeMode()
+    @objc func changeMode()
     {
         
         if let window = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-                if #available(iOS 15.0, *) {
-                    let windows = window.windows.first
-                        windows?.overrideUserInterfaceStyle = self.flag  == true ? .dark : .light
-                        flag = !flag
-                    
-                    }
-                } else if let window = UIApplication.shared.windows.first {
-                    if #available(iOS 13.0, *) {
-                            window.overrideUserInterfaceStyle = self.flag == true ? .dark : .light
-                        flag = !flag
-                    } else { //IOS 13 미만은 dark모드 불가
-                        window.overrideUserInterfaceStyle = .light
-                    }
-                }
+            if #available(iOS 15.0, *) {
+                let windows = window.windows.first
+                windows?.overrideUserInterfaceStyle = self.flag  == true ? .dark : .light
+                flag = !flag
+                
+            }
+        } else if let window = UIApplication.shared.windows.first {
+            if #available(iOS 13.0, *) {
+                window.overrideUserInterfaceStyle = self.flag == true ? .dark : .light
+                flag = !flag
+            } else { //IOS 13 미만은 dark모드 불가
+                window.overrideUserInterfaceStyle = .light
+            }
+        }
     }
     
-
-    
+    func configureTopBar(){
   
-    
-    //- MARK: Actions
-    @IBAction func changeModeButton(_ sender:UIButton)
-    {
-        changeMode()
+        
+        //navigationController?.navigationBar.tintColor = UIColor(named: "iconColor")!
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "list.dash"),style: .plain, target: self, action: #selector(changeMode))
+        
     }
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -49,14 +59,15 @@ class MainViewController: UIViewController {
     //- MARK: ViewMethod
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTopBar()
         // Do any additional setup after loading the view.
     }
     
     
-  
     
     
-
-  
+    
+    
+    
     
 }
