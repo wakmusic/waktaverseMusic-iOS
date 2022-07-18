2839adb50dc64f7c100c6eee00f5c289650ce6f0
yongbeomkwak <yongbeomkwak@gmail.com>
2022-07-18 22:52:57 +0900

    Change UI: Storyboard -> swift UI

---
+++ Billboardoo/Billboardoo/BillboardooApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillboardooApp.swift
+//  Billboardoo
+//
+//  Created by yongbeomkwak on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct BillboardooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
