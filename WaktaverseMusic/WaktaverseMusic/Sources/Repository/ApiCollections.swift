a4a759c4b14c5d78261022e7bfb27e6e55c3d481
yongbeomkwak <yongbeomkwak@gmail.com>
2022-10-14 00:35:56 +0900

    [CHORE] URL 변경
    - biilboard -> base url

--- WaktaverseMusic/WaktaverseMusic/Sources/Repository/ApiCollections.swift
+++ WaktaverseMusic/WaktaverseMusic/Sources/Repository/ApiCollections.swift
@@ -10,54 +10,50 @@ import Foundation
 class ApiCollections {
 
     // - MARK: top 100
-    static let totalTop100: String = "https://billboardoo.com/api/charts/total/100" // 누적 top 100
-    static let timeTop100: String = "https://billboardoo.com/api/charts/hourly/100" // 실시간 top 100
-    static let dailyTop100: String = "https://billboardoo.com/api/charts/daily/100" // 일간 top 100
-    static let weaklyTop100: String = "https://billboardoo.com/api/charts/weekly/100" // 주간 top 100
-    static let monthlyTop100: String = "https://billboardoo.com/api/charts/monthly/100" // 월간 top 100
+    static let totalTop100: String = "\(Const.URL.base)/api/charts/total/100" // 누적 top 100
+    static let timeTop100: String = "\(Const.URL.base)/api/charts/hourly/100" // 실시간 top 100
+    static let dailyTop100: String = "\(Const.URL.base)/api/charts/daily/100" // 일간 top 100
+    static let weaklyTop100: String = "\(Const.URL.base)/api/charts/weekly/100" // 주간 top 100
+    static let monthlyTop100: String = "\(Const.URL.base)/api/charts/monthly/100" // 월간 top 100
 
     // - MARK: new Songs
-    static let newWeekly: String = "https://billboardoo.com/api/charts/new/weekly" // 이주의 신곡
-    static let newMonthly: String =  "https://billboardoo.com/api/charts/new/monthly" // 이달의 신곡
+    static let newWeekly: String = "\(Const.URL.base)/api/charts/new/weekly" // 이주의 신곡
+    static let newMonthly: String =  "\(Const.URL.base)/api/charts/new/monthly" // 이달의 신곡
 
-    // "https://billboardoo.com/api/charts/new/monthly/220501"
+    // "\(Const.URL.base)/api/charts/new/monthly/220501"
 
     // - MARK: monthly, yearly
-    static let monthly: String = "https://billboardoo.com/api/charts/new/monthly/" // 월별 노래
-    static let yearly: String = "https://billboardoo.com/api/charts/new/yearly/" // 연도별 노래
+    static let monthly: String = "\(Const.URL.base)/api/charts/new/monthly/" // 월별 노래
+    static let yearly: String = "\(Const.URL.base)/api/charts/new/yearly/" // 연도별 노래
 
     // - MARK: newest
-    static let newest: String = "https://billboardoo.com/api/charts/new" // 최신곡
+    static let newest: String = "\(Const.URL.base)/api/charts/new" // 최신곡
 
     // - MARK: others
 
-    static let news: String = "https://billboardoo.com/api/news" // news
-    static let newsThumbnail: String = "https://billboardoo.com/news/thumbnail/"
+    static let news: String = "\(Const.URL.base)/api/news" // news
+    static let newsThumbnail: String = "\(Const.URL.base)/news/thumbnail/"
     static let newsCafe: String = "https://cafe.naver.com/steamindiegame/"
-    static let videos: String = "https://billboardoo.com/api/videos" // videos
+    static let videos: String = "\(Const.URL.base)/api/videos" // videos
 
-    static let artist: String = "https://billboardoo.com/api/artists/" // +artist id
-    static let albums: String = "https://billboardoo.com/api/albums/" // +artist id
+    static let artist: String = "\(Const.URL.base)/api/artists/" // +artist id
+    static let albums: String = "\(Const.URL.base)/api/albums/" // +artist id
 
-    static let searchOneSong: String = "https://billboardoo.com/api/charts/search/id/" // id
-    static let searchSongs: String = "https://billboardoo.com/api/charts/search/ids/" // ids (아이디1,아이디2,아이디3,...)
+    static let searchOneSong: String = "\(Const.URL.base)/api/charts/search/id/" // id
+    static let searchSongs: String = "\(Const.URL.base)/api/charts/search/ids/" // ids (아이디1,아이디2,아이디3,...)
 
-    static let searchTitleOrArtiest: String = "https://billboardoo.com/api/charts/search/keyword/" // (제목 또는 아티스트명)
+    static let searchTitleOrArtiest: String = "\(Const.URL.base)/api/charts/search/keyword/" // (제목 또는 아티스트명)
 
-    static let chartUpdateTotal: String = "https://billboardoo.com/api/charts/update/total"
+    static let chartUpdateTotal: String = "\(Const.URL.base)/api/charts/update/total"
 
-    static let chartUpdateHourly: String = "https://billboardoo.com/api/charts/update/hourly"
+    static let chartUpdateHourly: String = "\(Const.URL.base)/api/charts/update/hourly"
 
-    static let chartUpdateDaily: String = "https://billboardoo.com/api/charts/update/daily"
+    static let chartUpdateDaily: String = "\(Const.URL.base)/api/charts/update/daily"
 
-    static let chartUpdateWeekly: String = "https://billboardoo.com/api/charts/update/weekly"
+    static let chartUpdateWeekly: String = "\(Const.URL.base)/api/charts/update/weekly"
 
-    static let chartUpdateMonthly: String = "https://billboardoo.com/api/charts/update/monthly"
+    static let chartUpdateMonthly: String = "\(Const.URL.base)/api/charts/update/monthly"
 
-    static let tiwtch: String = "https://billboardoo.com/auth/login/twitch"
-    static let google: String = "https://billboardoo.com/auth/login/google"
-    static let naver: String = "https://billboardoo.com/auth/login/naver"
 
-    static let verify: String = "https://billboardoo.com/auth/api"
 
 }
